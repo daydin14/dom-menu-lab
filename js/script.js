d3d217c816861d312d1b64008b25904492bcfb28
David Aydin <david.aydin6@gmail.com>
2022-05-01 23:12:36 -0400

    Task 2.0-2.3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
-mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.innerHTML = "SEI Rocks!";
 mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.querySelector("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
