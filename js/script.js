33c94e7da703a5b8f7b9d4f191940bae9993118b
David Aydin <david.aydin6@gmail.com>
2022-05-03 11:33:48 -0400

    Task 4.4

--- js/script.js
+++ js/script.js
@@ -28,3 +28,4 @@ const subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
